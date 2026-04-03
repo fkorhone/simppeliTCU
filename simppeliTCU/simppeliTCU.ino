0fab34fe1f4be8ac9dc0fe1c459c6a16eb47a678
Frans Korhonen <frans.korhonen@gmail.com>
2026-04-03 10:36:14 +0300

    Report IP address to console when Wi-Fi starts

--- simppeliTCU/simppeliTCU.ino
+++ simppeliTCU/simppeliTCU.ino
@@ -176,8 +176,12 @@ void handleChargeOn() {
 void setup() {
   Serial.begin(115200);
   
+  Serial.print("Yhdistetään verkkoon ");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) { delay(500); }
+  Serial.println("\nWi-Fi yhdistetty!");
+  Serial.print("Löydät käyttöliittymän selaimella osoitteesta: http://");
+  Serial.println(WiFi.localIP());
   
   twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
   twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT((gpio_num_t)CAN_TX_PIN, (gpio_num_t)CAN_RX_PIN, TWAI_MODE_NORMAL);
